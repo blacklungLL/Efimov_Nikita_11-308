1e668289dd6418ad66966a2f211408e50498bf10
overmann <nikitosin2005@gmail.com>
2024-11-14 14:59:51 +0300

    added hw01 folder and made homework

--- hw01/src/hw01.cpp
+++ hw01/src/hw01.cpp
@@ -2,6 +2,36 @@
 
 int hw01(int start, int end)
 {
-    // Write code here..
+  if (start < 10 || start > 99 || end < 10 || end > 99 || start > end) {
     return -1;
+  }
+
+  int total = 0;
+  for (int i = start; i <= end; ++i) {
+    bool prime = true;
+    if (i < 2)
+    {
+        prime = false;
+    }
+    for (int j = 2; j * j <= i; ++j)
+    {
+      if (i % j == 0)
+      {
+        prime = false;
+        break;
+      }
+    }
+
+    if (prime) {
+      int summary = 0;
+      int num = i;
+      while (num > 0) {
+        summary += num % 10;
+        num /= 10;
+      }
+      total += summary;
+    }
+  }
+
+  return total;
 }
